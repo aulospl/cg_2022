bd9eda4269185a317aaaa50fb46798c1771c023b
Aulos Plautius <aulospl@gmail.com>
2022-04-27 20:15:26 -0300

    transformações aplicadas apenas nos objetos corretos

--- main.py
+++ main.py
@@ -38,7 +38,10 @@ def draw_circle_right(radius,position_x,position_y):
     
     return vertices
 
-
+iden = np.array([          1.0, 0.0, 0.0, 0.0, 
+                                    0.0, 1.0, 0.0, 0.0, 
+                                    0.0, 0.0, 1.0, 0.0, 
+                                    0.0, 0.0, 0.0, 1.0], np.float32)
 
 f = True
 tx = 0
@@ -47,36 +50,45 @@ sx = 1
 sy = 1
 d = 0
 rf = True
+c = 0
+signal = False
 def key_event(window,key,scancode,action,mods):
-    global f, tx, sy, d, rf
+    global f, tx, sy, d, rf,c, signal
+  
+    if c > 53:
+        print("FIM!")
+        return 1
     if f and action == glfw.PRESS and key == glfw.KEY_RIGHT:
-        print("right")
+        signal = True
         f = False
-        tx  += 0.01
-        sy -= 0.005
+        c += 1
+        sy -= 0.018518519
         if d == -45:
             rf = True
         elif d == 0:
             rf = False
         if rf:
-            d += 1
+            d += 5
+            tx  -= 0.02
         else:
-            d -= 1
-        print(d)
+            tx  += 0.01
+            d -= 5
     elif (not f) and action == glfw.PRESS and key == glfw.KEY_LEFT:
-        print("left")
+        signal = True
         f = True
-        sy -= 0.005
+        c += 1
+        sy -= 0.018518519
         tx += 0.01
         if d == -45:
             rf = True
         elif d == 0:
             rf = False
         if rf:
-            d += 1
+            d += 5
+            tx  -= 0.02
         else:
-            d -= 1
-        print(d)
+            d -= 5
+            tx  += 0.01
     
 
 def shaders_config():
@@ -168,7 +180,7 @@ if __name__ == "__main__":
 
     while not glfw.window_should_close(window):
 
-    
+        signal = False
         # funcao interna do glfw para gerenciar eventos de mouse, teclado, etc
         glfw.poll_events() 
         # limpa a cor de fundo da janela e preenche com outra no sistema RGBA
@@ -188,22 +200,45 @@ if __name__ == "__main__":
                                     0.0, sy, 0.0, 0.0, 
                                     0.0, 0.0, 1.0, 0.0, 
                                     0.0, 0.0, 0.0, 1.0], np.float32)
+        
+      
 
         water_scale = glGetUniformLocation(program, "mat")
         glUniformMatrix4fv(water_scale, 1, GL_TRUE, esc)
         water_color = glGetUniformLocation(program, "color")
         glUniform4f(water_color,0, 0, 1, 0)
         glDrawArrays(GL_TRIANGLE_STRIP, len(tank), len(water))
+        glUniformMatrix4fv(water_scale, 1, GL_TRUE, iden)
         # handle draw
+
+        hand_trans = np.array([     1.0, 0.0, 0.0, 0.35, 
+                                    0.0, 1.0, 0.0, 0.45, 
+                                    0.0, 0.0, 1.0, 0.0, 
+                                    0.0, 0.0, 0.0, 1.0], np.float32)
+
         rot = np.array([            matrixops.cos(-d), -matrixops.sin(-d), 0.0, 0.0, 
                                     matrixops.sin(-d), matrixops.cos(-d), 0.0, 0.0, 
                                     0.0, 0.0, 1.0, 0.0, 
                                     0.0, 0.0, 0.0, 1.0], np.float32)
         handle_rot = glGetUniformLocation(program, "mat")
-        glUniformMatrix4fv(handle_rot, 1, GL_TRUE, rot)
+        
+        if signal:
+            glUniformMatrix4fv(handle_rot, 1, GL_TRUE, iden)
+            glUniformMatrix4fv(handle_rot, 1, GL_TRUE, rot)
+            glUniformMatrix4fv(handle_rot, 1, GL_TRUE, iden)
+        else:
+            glUniformMatrix4fv(handle_rot, 1, GL_TRUE, rot)
+            pass
+
         handle_color = glGetUniformLocation(program, "color")
         glUniform4f(handle_color,0, 0, 0, 0)
+
         glDrawArrays(GL_TRIANGLE_STRIP, len(tank) + len(water), len(handle))
+
+        
+        handle_rot2 = glGetUniformLocation(program, "mat")
+        glUniformMatrix4fv(handle_rot2, 1, GL_TRUE, iden)
+
         # machine draw
         glDrawArrays(GL_TRIANGLE_FAN, len(tank)+ len(water) +len(handle), len(machine))
         # piston draw
@@ -258,6 +293,7 @@ if __name__ == "__main__":
         kirby_arm_color = glGetUniformLocation(program, "color")
         glUniform4f(kirby_arm_color,1, 0.43, 0.78, 0)
         glDrawArrays(GL_TRIANGLE_STRIP,len(tank)+ len(water)+ len(handle) + len(machine) + len(tree) + len(piston) + len(tree_top) + len(pipe_tank) + len(pipe_tree) + len(kirby),len(kirbyarm))
+        glUniformMatrix4fv(kirby_arm_translattion, 1, GL_TRUE, iden)
         # gerencia troca de dados entre janela e o OpenGL
         glfw.swap_buffers(window)
     
