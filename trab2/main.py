67d71f0c745f1dae2b06b3762a485db674d42526
Aulos Plautius <aulospl@gmail.com>
2022-06-22 15:14:44 -0300

    basico

--- trab2/main.py
+++ trab2/main.py
@@ -14,6 +14,128 @@ window = glfw.create_window(largura, altura, "Trabalho 2", None, None)
 glfw.make_context_current(window)
 
 
-while not glfw.window_should_close(window):
-    pass
-glfw.terminate()
+vertex_code = """
+        attribute vec3 position;
+        attribute vec2 texture_coord;
+        varying vec2 out_texture;
+                
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;        
+        
+        void main(){
+            gl_Position = projection * view * model * vec4(position,1.0);
+            out_texture = vec2(texture_coord);
+        }
+        """
+
+fragment_code = """
+        uniform vec4 color;
+        varying vec2 out_texture;
+        uniform sampler2D samplerTexture;
+        
+        void main(){
+            vec4 texture = texture2D(samplerTexture, out_texture);
+            gl_FragColor = texture;
+        }
+        """
+
+program  = glCreateProgram()
+vertex   = glCreateShader(GL_VERTEX_SHADER)
+fragment = glCreateShader(GL_FRAGMENT_SHADER)
+
+glShaderSource(vertex, vertex_code)
+glShaderSource(fragment, fragment_code)
+
+glCompileShader(vertex)
+if not glGetShaderiv(vertex, GL_COMPILE_STATUS):
+    error = glGetShaderInfoLog(vertex).decode()
+    print(error)
+    raise RuntimeError("Erro de compilacao do Vertex Shader")
+
+glCompileShader(fragment)
+if not glGetShaderiv(fragment, GL_COMPILE_STATUS):
+    error = glGetShaderInfoLog(fragment).decode()
+    print(error)
+    raise RuntimeError("Erro de compilacao do Fragment Shader")
+
+glAttachShader(program, vertex)
+glAttachShader(program, fragment)
+
+glLinkProgram(program)
+if not glGetProgramiv(program, GL_LINK_STATUS):
+    print(glGetProgramInfoLog(program))
+    raise RuntimeError('Linking error')
+    
+glUseProgram(program)
+
+
+def load_model_from_file(filename):
+    """Loads a Wavefront OBJ file. """
+    objects = {}
+    vertices = []
+    texture_coords = []
+    faces = []
+
+    material = None
+
+    # abre o arquivo obj para leitura
+    for line in open(filename, "r"): ## para cada linha do arquivo .obj
+        if line.startswith('#'): continue ## ignora comentarios
+        values = line.split() # quebra a linha por espaço
+        if not values: continue
+
+
+        ### recuperando vertices
+        if values[0] == 'v':
+            vertices.append(values[1:4])
+
+
+        ### recuperando coordenadas de textura
+        elif values[0] == 'vt':
+            texture_coords.append(values[1:3])
+
+        ### recuperando faces 
+        elif values[0] in ('usemtl', 'usemat'):
+            material = values[1]
+        elif values[0] == 'f':
+            face = []
+            face_texture = []
+            for v in values[1:]:
+                w = v.split('/')
+                face.append(int(w[0]))
+                if len(w) >= 2 and len(w[1]) > 0:
+                    face_texture.append(int(w[1]))
+                else:
+                    face_texture.append(0)
+
+            faces.append((face, face_texture, material))
+
+    model = {}
+    model['vertices'] = vertices
+    model['texture'] = texture_coords
+    model['faces'] = faces
+
+    return model
+
+glEnable(GL_TEXTURE_2D)
+qtd_texturas = 10
+textures = glGenTextures(qtd_texturas)
+
+def load_texture_from_file(texture_id, img_textura):
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    img = Image.open(img_textura)
+    img_width = img.size[0]
+    img_height = img.size[1]
+    image_data = img.tobytes("raw", "RGB", 0, -1)
+    #image_data = np.array(list(img.getdata()), np.uint8)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
+
+vertices_list = []    
+textures_coord_list = []
+
+# Carregar modelos e aplicar texturas
