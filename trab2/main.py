45e939c0f0cbc0d27b15365a3a52f50bc8a3526a
Aulos Plautius <aulospl@gmail.com>
2022-06-24 14:32:44 -0300

    esqueleto do codigo pronto

--- trab2/main.py
+++ trab2/main.py
@@ -78,7 +78,6 @@ def load_model_from_file(filename):
     faces = []
 
     material = None
-
     # abre o arquivo obj para leitura
     for line in open(filename, "r"): ## para cada linha do arquivo .obj
         if line.startswith('#'): continue ## ignora comentarios
@@ -139,3 +138,212 @@ vertices_list = []
 textures_coord_list = []
 
 # Carregar modelos e aplicar texturas
+modelo = load_model_from_file('terreno/terreno2.obj')
+
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo terreno.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]:
+        vertices_list.append( modelo['vertices'][vertice_id-1] )
+    for texture_id in face[1]:
+        textures_coord_list.append( modelo['texture'][texture_id-1] )
+print('Processando modelo terreno.obj. Vertice final:',len(vertices_list))
+
+load_texture_from_file(1,'terreno/pedra.jpg')
+
+buffer = glGenBuffers(2)
+
+vertices = np.zeros(len(vertices_list), [("position", np.float32, 3)])
+vertices['position'] = vertices_list
+
+# Upload data
+glBindBuffer(GL_ARRAY_BUFFER, buffer[0])
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+stride = vertices.strides[0]
+offset = ctypes.c_void_p(0)
+loc_vertices = glGetAttribLocation(program, "position")
+glEnableVertexAttribArray(loc_vertices)
+glVertexAttribPointer(loc_vertices, 3, GL_FLOAT, False, stride, offset)
+
+textures = np.zeros(len(textures_coord_list), [("position", np.float32, 2)]) # duas coordenadas
+textures['position'] = textures_coord_list
+
+
+# Upload data
+glBindBuffer(GL_ARRAY_BUFFER, buffer[1])
+glBufferData(GL_ARRAY_BUFFER, textures.nbytes, textures, GL_STATIC_DRAW)
+stride = textures.strides[0]
+offset = ctypes.c_void_p(0)
+loc_texture_coord = glGetAttribLocation(program, "texture_coord")
+glEnableVertexAttribArray(loc_texture_coord)
+glVertexAttribPointer(loc_texture_coord, 2, GL_FLOAT, False, stride, offset)
+
+def desenha_terreno():
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 0.0;
+    r_x = 0.0; r_y = 0.0; r_z = 1.0;
+    
+    # translacao
+    t_x = 0.0; t_y = -1.01; t_z = 0.0;
+    
+    # escala
+    s_x = 20.0; s_y = 20.0; s_z = 20.0;
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    #define id da textura do modelo
+    glBindTexture(GL_TEXTURE_2D, 1)
+    
+    
+    # desenha o modelo
+    glDrawArrays(GL_TRIANGLES, 36, 42-36) ## renderizando
+
+cameraPos   = glm.vec3(0.0,  0.0,  1.0);
+cameraFront = glm.vec3(0.0,  0.0, -1.0);
+cameraUp    = glm.vec3(0.0,  1.0,  0.0);
+
+
+polygonal_mode = False
+
+def key_event(window,key,scancode,action,mods):
+    global cameraPos, cameraFront, cameraUp, polygonal_mode
+    
+    cameraSpeed = 0.2
+    if key == 87 and (action==1 or action==2): # tecla W
+        cameraPos += cameraSpeed * cameraFront
+    
+    if key == 83 and (action==1 or action==2): # tecla S
+        cameraPos -= cameraSpeed * cameraFront
+    
+    if key == 65 and (action==1 or action==2): # tecla A
+        cameraPos -= glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+    if key == 68 and (action==1 or action==2): # tecla D
+        cameraPos += glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+    if key == 80 and action==1 and polygonal_mode==True:
+        polygonal_mode=False
+    else:
+        if key == 80 and action==1 and polygonal_mode==False:
+            polygonal_mode=True
+        
+        
+        
+firstMouse = True
+yaw = -90.0 
+pitch = 0.0
+lastX =  largura/2
+lastY =  altura/2
+
+def mouse_event(window, xpos, ypos):
+    global firstMouse, cameraFront, yaw, pitch, lastX, lastY
+    if firstMouse:
+        lastX = xpos
+        lastY = ypos
+        firstMouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+    lastX = xpos
+    lastY = ypos
+
+    sensitivity = 0.3 
+    xoffset *= sensitivity
+    yoffset *= sensitivity
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    
+    if pitch >= 90.0: pitch = 90.0
+    if pitch <= -90.0: pitch = -90.0
+
+    front = glm.vec3()
+    front.x = math.cos(glm.radians(yaw)) * math.cos(glm.radians(pitch))
+    front.y = math.sin(glm.radians(pitch))
+    front.z = math.sin(glm.radians(yaw)) * math.cos(glm.radians(pitch))
+    cameraFront = glm.normalize(front)
+
+
+    
+glfw.set_key_callback(window,key_event)
+glfw.set_cursor_pos_callback(window, mouse_event)
+
+def model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z):
+    
+    angle = math.radians(angle)
+    
+    matrix_transform = glm.mat4(1.0) # instanciando uma matriz identidade
+
+    
+    # aplicando translacao
+    matrix_transform = glm.translate(matrix_transform, glm.vec3(t_x, t_y, t_z))    
+    
+    # aplicando rotacao
+    matrix_transform = glm.rotate(matrix_transform, angle, glm.vec3(r_x, r_y, r_z))
+    
+    # aplicando escala
+    matrix_transform = glm.scale(matrix_transform, glm.vec3(s_x, s_y, s_z))
+    
+    matrix_transform = np.array(matrix_transform)
+    
+    return matrix_transform
+
+def view():
+    global cameraPos, cameraFront, cameraUp
+    mat_view = glm.lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    mat_view = np.array(mat_view)
+    return mat_view
+
+def projection():
+    global altura, largura
+    # perspective parameters: fovy, aspect, near, far
+    mat_projection = glm.perspective(glm.radians(45.0), largura/altura, 0.1, 1000.0)
+    mat_projection = np.array(mat_projection)    
+    return mat_projection
+
+glfw.show_window(window)
+glfw.set_cursor_pos(window, lastX, lastY)
+
+glEnable(GL_DEPTH_TEST) ### importante para 3D
+   
+
+rotacao_inc = 0
+while not glfw.window_should_close(window):
+
+    glfw.poll_events() 
+    
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    glClearColor(1.0, 1.0, 1.0, 1.0)
+    
+    if polygonal_mode==True:
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
+    if polygonal_mode==False:
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
+    
+    
+
+    desenha_terreno()
+
+
+    
+    mat_view = view()
+    loc_view = glGetUniformLocation(program, "view")
+    glUniformMatrix4fv(loc_view, 1, GL_TRUE, mat_view)
+
+    mat_projection = projection()
+    loc_projection = glGetUniformLocation(program, "projection")
+    glUniformMatrix4fv(loc_projection, 1, GL_TRUE, mat_projection)    
+    
+    
+
+    
+    glfw.swap_buffers(window)
+
+glfw.terminate()
